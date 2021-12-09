9c94d7accf8cffe7d0360ecd44b0b0cf21d27f7f
Ricky Pardomuan Hutagalung <programingteam914@gmail.com>
2021-12-10 00:26:32 +0700

    V3

--- Assets/Script/UIScript.cs
+++ Assets/Script/UIScript.cs
@@ -8,6 +8,47 @@ public class UIScript : MonoBehaviour
    {
        SceneManager.LoadScene(1);
    }
+   public void level1_1 ()
+   {
+       SceneManager.LoadScene(2);
+   }
+   public void level1_2 ()
+   {
+       SceneManager.LoadScene(3);
+   }
+   public void level1_3 ()
+   {
+       SceneManager.LoadScene(4);
+   }
+   public void level1_4 ()
+   {
+       SceneManager.LoadScene(5);
+   }
+   public void level1_5()
+   {
+       SceneManager.LoadScene(6);
+   }
+   public void level2_1()
+   {
+       SceneManager.LoadScene(7);
+   }
+   public void level2_2()
+   {
+       SceneManager.LoadScene(8);
+   }
+   public void level2_3()
+   {
+       SceneManager.LoadScene(9);
+   }
+   public void level2_4()
+   {
+       SceneManager.LoadScene(10);
+   }
+   public void level2_5()
+   {
+       SceneManager.LoadScene(11);
+   }
+
 
    public void Quit()
    {
