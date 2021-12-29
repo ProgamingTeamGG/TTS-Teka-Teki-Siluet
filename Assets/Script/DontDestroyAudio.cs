6ca2876dad0e6a38875d663f821653c39c4f5cc4
rijalkhoerulumam <94157584+ProgamingTeamGG@users.noreply.github.com>
2021-12-29 16:34:48 +0700

    Riku
    Volume dan BGM telah ditambahkan

--- Assets/Script/DontDestroyAudio.cs
+++ Assets/Script/DontDestroyAudio.cs
@@ -9,6 +9,11 @@ public class DontDestroyAudio : MonoBehaviour
    
      void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject); 
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if(musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject); 
     }
 }
